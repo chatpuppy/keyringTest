dc2edc6fc947af08622fad9cf482c6a26d6e9253
chatpuppy <ydfuf1dn@gmail.com>
2022-12-16 17:47:40 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import * as passworder from '@metamask/browser-passworder';
+import KeyringController from 'eth-keyring-controller';
+import SimpleKeyring from 'eth-simple-keyring';
+
+window.Buffer = window.Buffer || require("buffer").Buffer; 
 
 function App() {
+	const secrets = { coolStuff: 'all', ssn: 'livin large' };
+	const password = 'hunter55';
+	const seed = "meat admit ivory unfold pistol alley work vault tilt witness lion talent";
+	const address = "0x6756fB068c3Cce14DA7e32Ed666F00BEa8486958";
+
+	const removeAccount = () => {
+
+	}
+
+	const keyringController = new KeyringController({
+		keyringTypes: [SimpleKeyring], // optional array of types to support.
+		// initState: initState.KeyringController, // Last emitted persisted state.
+		encryptor: undefined,
+	});
+
+	// The KeyringController is also an event emitter:
+	keyringController.on('newAccount', (address: string) => {
+		console.log(`New account created: ${address}`);
+	});
+	
+	keyringController.on('removedAccount', removeAccount);
+
+
+	const testEncryptAndDecrypt = () => {
+		passworder
+		.encrypt(password, secrets)
+		.then(function (blob: any) {
+			return passworder.decrypt(password, blob);
+		})
+		.then(function (result: any) {
+			console.log(result);
+		});
+	}
+
+	const createNewVaultAndRestore = async () => {
+		const result = await keyringController.createNewVaultAndRestore(password, seed);
+		console.log('createNewVaultAndRestore result', result);
+	}
+
+	const submitPassword = async (pwd: string) => {
+		const result = await keyringController.submitPassword(pwd);
+		console.log('submitPassword result', result);
+	}
+
+	const createKeyring = async (e: any) => {
+		testEncryptAndDecrypt();
+		await createNewVaultAndRestore();
+	}
+	
+	const unlock = async (e: any) => {
+		// await submitPassword(password + "000"); // wrong password
+		await submitPassword(password);
+	}
+
+	const verifyPassword = async (e: any) => {
+		const result = await keyringController.verifyPassword(password);
+		console.log("verifyPassword true");
+	}
+
+	const addNewKeyring = async (e: any) => {
+
+	}
+
+	const addNewAccount = async (e: any) => {
+
+	}
+
+	const getKeyringForAccount = async (e: any) => {
+		const result = await keyringController.getKeyringForAccount(address);
+		console.log("getKeyringForAccount result", result);
+	}
+
+	const getEncryptionPublicKey = async (e: any) => {
+		const publicKey = await keyringController.getEncryptionPublicKey(address);
+		console.log("getEncryptionPublicKey result", publicKey);
+	}
+
+	const setLocked = async (e: any) => {
+		await keyringController.setLocked();
+		console.log("wallet locked");
+	}
+
+	const getPrivateKey = async (e: any) => {
+		const keyring = await keyringController.getKeyringForAccount(address);
+		const priKey = keyring.wallets[0].privateKey;
+		console.log("Private key", priKey);
+	}
+	
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+				<div className="button" onClick={(e) => createKeyring(e)}>Initialize wallet</div>
+				<div className="button" onClick={(e) => setLocked(e)}>Lock wallet</div>
+				<div className="button" onClick={(e) => unlock(e)}>Unlock wallet</div>
+				<div className="button" onClick={(e) => verifyPassword(e)}>Verify password</div>
+				<div className="button" onClick={(e) => addNewKeyring(e)}>Add a new keyring</div>
+				<div className="button" onClick={(e) => addNewAccount(e)}>Add a new account</div>
+				<div className="button" onClick={(e) => getKeyringForAccount(e)}>Get keyring by address</div>
+				<div className="button" onClick={(e) => getEncryptionPublicKey(e)}>Get public key by address</div>
+				<div className="button" onClick={(e) => getPrivateKey(e)}>Get private key for 1st address</div>
+				
+								
       </header>
     </div>
   );
